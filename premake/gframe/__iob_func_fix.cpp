911d4b8c96e83b5a3041cc40547c891709b166c6
mercury233 <me@mercury233.me>
2017-11-27 21:07:48 +0800

    comment

--- premake/gframe/__iob_func_fix.cpp
+++ premake/gframe/__iob_func_fix.cpp
@@ -1,3 +1,6 @@
+// fix irrklang static lib don't support VS2015
+// http://blog.csdn.net/10km/article/details/50528908
+
 #if _MSC_VER >= 1900
 #include "stdio.h" 
 _ACRTIMP_ALT FILE* __cdecl __acrt_iob_func(unsigned);
