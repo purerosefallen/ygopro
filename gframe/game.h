b4fffe33a5a7b322185097687356b15ea326c992
purerosefallen <78877@qq.com>
2018-04-07 16:14:11 +0800

    spec

--- gframe/game.h
+++ gframe/game.h
@@ -657,7 +657,6 @@ extern Game* mainGame;
 #define BUTTON_MARKS_FILTER			380
 #define BUTTON_MARKERS_OK			381
 
-
 #define TEXTURE_DUEL				0
 #define TEXTURE_DECK				1
 #define TEXTURE_MENU				2
@@ -665,8 +664,6 @@ extern Game* mainGame;
 #define TEXTURE_COVER_O				4
 #define TEXTURE_ATTACK				5
 #define TEXTURE_ACTIVATE			6
-
-
 
 #define DEFAULT_DUEL_RULE			4
 #endif // GAME_H
