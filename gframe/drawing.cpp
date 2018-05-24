95a75127e3b7a237da1f14b1488b105c95a6833f
purerosefallen <78877@qq.com>
2018-05-24 13:26:22 +0800

    format

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -558,7 +558,7 @@ void Game::DrawMisc() {
 	}
 	DrawShadowText(numFont, dInfo.strLP[0], Resize(330, 12, 631, 30), Resize(0, 1, 2, 0), 0xffffff00, 0xff000000, true, false, 0);
 	DrawShadowText(numFont, dInfo.strLP[1], Resize(691, 12, 992, 30), Resize(0, 1, 2, 0), 0xffffff00, 0xff000000, true, false, 0);
-	
+
 	recti p1size = Resize(335, 31, 629, 50);
 	recti p2size = Resize(986, 31, 986, 50);
 	if(!dInfo.isTag || !dInfo.tag_player[0])
