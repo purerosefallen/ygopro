db43dc6d7834019f4ce0bc872345a6db3901e28e
purerosefallen <78877@qq.com>
2018-06-22 06:58:58 +0800

    updatepremake

--- gframe/premake4.lua
+++ gframe/premake4.lua
@@ -9,7 +9,7 @@ project "ygopro"
     links { "ocgcore", "clzma", "Irrlicht", "freetype", "sqlite3", "lua" , "event" }
     if USE_IRRKLANG then
         defines { "YGOPRO_USE_IRRKLANG" }
-        links { "irrKlang", "ikpmp3" }
+        links { "ikpmp3" }
         includedirs { "../irrklang/include" }
         if IRRKLANG_PRO then
             defines { "IRRKLANG_STATIC" }
@@ -24,6 +24,7 @@ project "ygopro"
         excludes "CGUIButton.cpp"
         includedirs { "../irrlicht/include", "../freetype/include", "../event/include", "../sqlite3" }
         if USE_IRRKLANG then
+            links { "irrKlang" }
             libdirs { "../irrklang/lib/Win32-visualStudio" }
         end
         links { "opengl32", "ws2_32", "winmm", "gdi32", "kernel32", "user32", "imm32" }
@@ -37,6 +38,12 @@ project "ygopro"
         links { "event_pthreads", "GL", "dl", "pthread" }
     configuration "linux"
         if USE_IRRKLANG then
+            links { "IrrKlang" }
             linkoptions{ "-Wl,-rpath=./" }
             libdirs { "../irrklang/bin/linux-gcc-64" }
+        end
+    configuration "macosx"
+        if USE_IRRKLANG then
+            links { "irrklang" }
+            libdirs { "../irrklang/bin/macosx-gcc" }
         end
