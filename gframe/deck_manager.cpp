069a6daa3e6fabb40b254a213ca564a370583ec2
purerosefallen <78877@qq.com>
2018-11-03 08:32:36 +0800

    catchup

--- gframe/deck_manager.cpp
+++ gframe/deck_manager.cpp
@@ -50,14 +50,14 @@ void DeckManager::LoadLFListSingle(const char* path) {
 	}
 }
 void DeckManager::LoadLFList() {
-	//LoadLFListSingle("expansions/lflist.conf");
+	LoadLFListSingle("expansions/lflist.conf");
 #ifdef _WIN32
 	char fpath[1000];
 	WIN32_FIND_DATAW fdataw;
 	HANDLE fh = FindFirstFileW(L"./expansions/*", &fdataw);
 	if(fh != INVALID_HANDLE_VALUE) {
 		do {
-			if((fdataw.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+			if(wcscmp(L".",fdataw.cFileName) != 0 && wcscmp(L"..",fdataw.cFileName) != 0 && fdataw.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 				char fname[780];
 				BufferIO::EncodeUTF8(fdataw.cFileName, fname);
 				sprintf(fpath, "./expansions/%s/lflist.conf", fname);
@@ -71,7 +71,7 @@ void DeckManager::LoadLFList() {
 	struct dirent * dirp;
 	if((dir = opendir("./expansions/")) != NULL) {
 		while((dirp = readdir(dir)) != NULL) {
-			if (dirp->d_type != DT_DIR)
+			if (strcmp(".", dirp->d_name) == 0 || strcmp("..", dirp->d_name) == 0 || dirp->d_type != DT_DIR)
 				continue;
 			char filepath[1000];
 			sprintf(filepath, "./expansions/%s/lflist.conf", dirp->d_name);
