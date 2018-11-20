70ede05e5375b95f9b223c1a6c888136f75a4ee3
mercury233 <me@mercury233.me>
2018-11-21 01:29:39 +0800

    test win sdk

--- premake5.lua
+++ premake5.lua
@@ -13,7 +13,7 @@ solution "ygo"
         libdirs { "$(DXSDK_DIR)Lib/x86" }
         entrypoint "mainCRTStartup"
         toolset "v141_xp"
-        systemversion "10.0.17763.0"
+        systemversion "latest"
         startproject "ygopro"
 
     configuration "bsd"
