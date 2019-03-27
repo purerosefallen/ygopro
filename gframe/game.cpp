de04eeee8417ee85c8e40996af3a132d1c938c90
purerosefallen <78877@qq.com>
2019-03-27 20:38:10 +0800

    Revert "Merge branch 'subdir'"
    This reverts commit a245573663ecc841f4a3e6e67eefc2d75d28db40, reversing
    changes made to 83f05fd7ae6f6a21d35f0bc520b8b479b145fe58.

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -89,7 +89,7 @@ bool Game::Initialize() {
 		ErrorLog("Failed to load strings!");
 		return false;
 	}
-	LoadExpansionStrings();
+	dataManager.LoadStrings("./expansions/strings.conf");
 	env = device->getGUIEnvironment();
 	numFont = irr::gui::CGUITTFont::createTTFont(env, gameConf.numfont, 16);
 	adFont = irr::gui::CGUITTFont::createTTFont(env, gameConf.numfont, 12);
@@ -982,31 +982,11 @@ void Game::SetStaticText(irr::gui::IGUIStaticText* pControl, u32 cWidth, irr::gu
 	pControl->setText(dataManager.strBuffer);
 }
 void Game::LoadExpansionDB() {
-	LoadExpansionDBDirectry("./expansions");
-	FileSystem::TraversalDir("./expansions", [this](const char* name, bool isdir) {
-		if(isdir && strcmp(name, ".") && strcmp(name, "..") && strcmp(name, "pics") && strcmp(name, "script")) {
-			char subdir[1024];
-			sprintf(subdir, "./expansions/%s", name);
-			LoadExpansionDBDirectry(subdir);
-		}
-	});
-}
-void Game::LoadExpansionDBDirectry(const char* path) {
-	FileSystem::TraversalDir(path, [path](const char* name, bool isdir) {
+	FileSystem::TraversalDir("./expansions", [](const char* name, bool isdir) {
 		if(!isdir && strrchr(name, '.') && !mystrncasecmp(strrchr(name, '.'), ".cdb", 4)) {
 			char fpath[1024];
-			sprintf(fpath, "%s/%s", path, name);
+			sprintf(fpath, "./expansions/%s", name);
 			dataManager.LoadDB(fpath);
-		}
-	});
-}
-void Game::LoadExpansionStrings() {
-	dataManager.LoadStrings("./expansions/strings.conf");
-	FileSystem::TraversalDir("./expansions", [](const char* name, bool isdir) {
-		if(isdir && strcmp(name, ".") && strcmp(name, "..") && strcmp(name, "pics") && strcmp(name, "script")) {
-			char fpath[1024];
-			sprintf(fpath, "./expansions/%s/strings.conf", name);
-			dataManager.LoadStrings(fpath);
 		}
 	});
 }
--- gframe/tag_duel.h
+++ gframe/tag_duel.h
@@ -28,7 +28,7 @@ public:
 	virtual void GetResponse(DuelPlayer* dp, void* pdata, unsigned int len);
 	virtual void TimeConfirm(DuelPlayer* dp);
 	virtual void EndDuel();
-
+	
 	void DuelEndProc();
 	void WaitforResponse(int playerid);
 	void RefreshMzone(int player, int flag = 0x881fff, int use_cache = 1);
@@ -40,7 +40,7 @@ public:
 
 	static int MessageHandler(long fduel, int type);
 	static void TagTimer(evutil_socket_t fd, short events, void* arg);
-
+	
 protected:
 	DuelPlayer* players[4];
 	DuelPlayer* pplayer[4];
