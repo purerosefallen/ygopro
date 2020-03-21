d77ebc206a3550f8b89b4750843bb6f7581778f3
Joy J <316351048@qq.com>
2020-03-22 00:11:05 +0800

    improve auto-watch mode

--- gframe/duelclient.cpp
+++ gframe/duelclient.cpp
@@ -2146,6 +2146,12 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 			pcard = *(mainGame->dField.deck[player].rbegin() + i);
 			if (code != 0)
 				pcard->SetCode(code);
+			if (auto_watch_mode && code > 0) {
+				mainGame->showcardcode = code;
+				mainGame->showcarddif = 0;
+				mainGame->showcardp = 0;
+				mainGame->showcard = 4;
+			}
 		}
 		if(mainGame->dInfo.isReplaySkiping)
 			return true;
@@ -2184,6 +2190,12 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 			pcard = *(mainGame->dField.extra[player].rbegin() + i + mainGame->dField.extra_p_count[player]);
 			if (code != 0)
 				pcard->SetCode(code);
+			if (auto_watch_mode && code > 0) {
+				mainGame->showcardcode = code;
+				mainGame->showcarddif = 0;
+				mainGame->showcardp = 0;
+				mainGame->showcard = 4;
+			}
 		}
 		if(mainGame->dInfo.isReplaySkiping)
 			return true;
@@ -2235,6 +2247,12 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 				pcard = mainGame->dField.GetCard(c, l, s);
 			if (code != 0)
 				pcard->SetCode(code);
+			if (auto_watch_mode && code > 0) {
+				mainGame->showcardcode = code;
+				mainGame->showcarddif = 0;
+				mainGame->showcardp = 0;
+				mainGame->showcard = 4;
+			}
 			mainGame->gMutex.lock();
 			myswprintf(textBuffer, L"*[%ls]", dataManager.GetName(code));
 			mainGame->AddLog(textBuffer, code);
@@ -3457,14 +3475,10 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 		int count = BufferIO::ReadInt16(pbuf);
 		ClientCard* pc = mainGame->dField.GetCard(c, l, s);
 		if (auto_watch_mode && pc->code > 0) {
-			myswprintf(event_string, dataManager.GetSysString(1610), dataManager.GetName(pc->code));
 			mainGame->showcardcode = pc->code;
 			mainGame->showcarddif = 0;
 			mainGame->showcardp = 0;
-			mainGame->showcard = 5;
-			mainGame->WaitFrameSignal(30);
-			mainGame->showcard = 0;
-			mainGame->WaitFrameSignal(11);
+			mainGame->showcard = 2;
 		}
 		if (pc->counters.count(type))
 			pc->counters[type] += count;
