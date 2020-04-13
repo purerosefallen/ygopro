afca6cd26eddca297a2eeeb2054bf69c2ad1040a
purerosefallen <78877@qq.com>
2020-04-13 16:42:12 +0800

    update premake5

--- premake5.lua
+++ premake5.lua
@@ -64,7 +64,7 @@ end
 if not os.ishost("macosx") then
         staticruntime "On"
 end
-        disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477", "4091", "4305", "4828", "4800" }
+        disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477", "4091", "4828", "4800" }
 
     configuration { "Release", "not vs*" }
         symbols "On"
