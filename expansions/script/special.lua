0b6ccb4f7fd9b6fb5a020ebd5c8564dd6a0becfa
Joy J <316351048@qq.com>
2020-05-28 21:49:22 +0800

    update special.lua

--- expansions/script/special.lua
+++ expansions/script/special.lua
@@ -35,22 +35,6 @@ function aux.CannotSetCond(e,c)
 end
 
 function aux.MZoneLimit(tp)
-	--[[
-	local e1=Effect.GlobalEffect()
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetCondition(aux.SummonCondition)
-	Duel.RegisterEffect(e1,tp)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_CANNOT_MSET)
-	Duel.RegisterEffect(e2,tp)
-	local e3=e1:Clone()
-	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e3:SetCondition(aux.TRUE)
-	e3:SetTarget(aux.SummonCondition)
-	Duel.RegisterEffect(e3,tp)]]--
 	local e4=Effect.GlobalEffect()
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetCode(EFFECT_DISABLE_FIELD)
@@ -85,68 +69,6 @@ function aux.SummonCondition(e,c,sump,sumtype,sumpos,targetp,se)
 	return Duel.IsExistingMatchingCard(aux.IsAtMainZone,e:GetHandlerPlayer(),LOCATION_MZONE,0,3,nil) and (not c or not c:IsLocation(LOCATION_EXTRA))
 end
 
-function aux.EffectNegate(tp)
-	--negate
-	local e1=Effect.GlobalEffect()
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAIN_SOLVING)
-	e1:SetOperation(aux.EffectNegateOperation)
-	Duel.RegisterEffect(e1,tp)
-end
-
-function aux.NeedEx(c)
-	return c:IsLocation(LOCATION_EXTRA) and (c:IsType(TYPE_PENDULUM) or c:IsType(TYPE_LINK))
-end
-
-function aux.CanToEx(c)
-	return c:IsLocation(LOCATION_EXTRA) and not (c:IsType(TYPE_PENDULUM) or c:IsType(TYPE_LINK))
-end
-
-function aux.NoneEffect(e)
-	if OriginalEffects[e] then e:SetOperation(OriginalEffects[e]) end
-end
-
-OriginalEffects = {}
-
-function aux.EffectNegateOperation(e,tp,eg,ep,ev,re,r,rp)
-	if not re:IsHasCategory(CATEGORY_SPECIAL_SUMMON) then return end
-	if re:GetHandlerPlayer()~=tp then return end
-	local a,g,count,dp,dv = Duel.GetOperationInfo(ev,CATEGORY_SPECIAL_SUMMON)
-	local code=re:GetHandler():GetOriginalCode()
-	if code==43434803 or code==58551308 or code==22404675 or code==99330325 then return end
-	local mainAvailable = 3-Duel.GetMatchingGroupCount(aux.IsAtMainZone,tp,LOCATION_MZONE,0,nil)
-	local extraAvailable = Duel.GetMatchingGroupCount(aux.IsExtraMonster,tp,LOCATION_MZONE,0,nil)==0
-	local needMain = 0
-	if g and aux.GetValueType(g)=="Card" then g=Group.FromCards(g) end
-	if g then
-		local g2 = g:Clone():Filter(aux.NeedEx,nil)
-		local g3 = g:Clone():Filter(aux.CanToEx,nil)
-		local needEx = g2:GetCount()
-		local canToEx = g3:GetCount()
-		needMain = g:GetCount() - g2:GetCount()
-		local mainMin = needMain
-		local mainMax = needMain + canToEx
-		local extraMin = needEx
-		local extraMax = needEx + canToEx
-		local exZoneCount = Duel.GetLocationCountFromEx(tp)
-		if exZoneCount >= extraMin and mainAvailable >= mainMin then return end
-	else
-		if count then
-			needMain = count
-			if dv == LOCATION_EXTRA and extraAvailable then needMain = needMain - 1 end
-		else
-			needMain = 0
-		end
-	end
-	if mainAvailable < needMain then
-		if not OriginalEffects[re] then
-			OriginalEffects[re] = re:GetOperation()
-		end
-		re:SetOperation(aux.NoneEffect)
-		Duel.Exile(re:GetHandler(),REASON_RULE)
-	end
-end
-
 function aux.AdjustRegist()
 	--adjust
 	local e1=Effect.GlobalEffect()
@@ -162,18 +84,6 @@ function aux.AdjustRegist()
 	e2:SetCode(EVENT_ADJUST)
 	e2:SetOperation(aux.AdjustOperationMonster)
 	Duel.RegisterEffect(e2,0)
-end
-
-function aux.IsExtraMonster(c)
-	return c:GetSequence()>=5
-end
-
-function aux.Kill(c)
-	if c:IsType(TYPE_TOKEN) then
-		Duel.Exile(c,REASON_RULE)
-	else
-		Duel.SendtoGrave(c)
-	end
 end
 
 function aux.AdjustOperationMonster(e,tp,eg,ep,ev,re,r,rp)
@@ -295,13 +205,66 @@ function aux.CheckDesDeck()
 	e=e3:Clone()
 	Duel.RegisterEffect(e,1)
 end
+function aux.CannotActivate(tp)
+	if not aux.SP4 then
+		aux.SP4={73915051,78785392}
+		aux.SP3={18027138,18322364,33298291,34707034,44097050,49808196,50750868,56051648,65664792,67949763,72537897,73199638,93224848,96383838,97433739,97926515}
+		aux.SP3Ex={7392745,29843091,42956963}
+		aux.SP2={1969506,6148016,7161742,9925982,9929398,10698416,10971759,10971759,13482262,13504844,14604710,15341821,15394083,17418744,17502671,20285786,21007444,21179143,22110647,23536866,29087919,31986288,33551032,39998992,40450317,40844552,41197012,41442341,43140791,44161893,46005939,46173679,48411996,51205763,51987571,55416843,56337500,56597272,57288708,58531587,58787301,60764581,60930169,61314842,61840587,61948106,62376646,64496451,66393507,71541986,71867500,73906480,74875003,75252099,76930964,77642288,77847678,79783880,80802524,81587028,82324105,86174055,92781606}
+		aux.SP2Ex={14470845,28062325,41141943,93749093,93775296}
+	end
+	--cannot activate
+	local e1=Effect.GlobalEffect()
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(aux.CannotActivateValue)
+	Duel.RegisterEffect(e1,tp)
+end
+function aux.IsTableContainValue(tbl,val)
+	for k,v in ipairs(tbl) do
+		if v==val then return true end
+	end
+	return false
+end
+function aux.CannotActivateValue(e,te,tp)
+	if tp~=e:GetHandlerPlayer() then return false end
+	local mon=Duel.GetMatchingGroupCount(aux.IsAtMainZone,tp,LOCATION_MZONE,0,nil)
+	local enemymon=Duel.GetMatchingGroupCount(aux.IsAtMainZone,tp,0,LOCATION_MZONE,nil)
+	local c=te:GetHandler()
+	if not c then return false end
+	local code=c:GetCode()
+	if aux.IsTableContainValue(aux.SP4,code) then return true end
+	if mon>0 and aux.IsTableContainValue(aux.SP3,code) then return true end
+	if enemymon>0 and aux.IsTableContainValue(aux.SP3Ex,code) then return true end
+	if mon>0 and code==53315891 and te:GetHandler():IsLocation(LOCATION_GRAVE) then return true end
+	if mon>1 and aux.IsTableContainValue(aux.SP2,code) then return true end
+	if enemymon>1 and aux.IsTableContainValue(aux.SP2Ex,code) then return true end
+	if mon>1 and code==22227683 and te:GetCode()==EVENT_BATTLE_DESTROYING then return true end
+	if mon>1 and code==25669282 and te:GetHandler():IsLocation(LOCATION_GRAVE) then return true end
+	if mon>1 and code==27198001 and te:GetCode()==EVENT_TO_GRAVE then return true end
+	if mon>1 and code==34767865 and te:GetRange()==LOCATION_GRAVE then return true end
+	if mon>1 and code==41933425 and te:GetCode()==EVENT_FREE_CHAIN then return true end
+	if mon>1 and code==43138260 and te:GetCode()==EVENT_SPSUMMON_SUCCESS then return true end
+	if mon>1 and code==48736598 and te:GetCode()==EVENT_SPSUMMON_SUCCESS then return true end
+	if mon>1 and code==53855409 and te:GetCode()==EVENT_BE_MATERIAL then return true end
+	if mon>1 and code==62481203 and te:GetCode()==EVENT_FREE_CHAIN then return true end
+	if mon>1 and code==66141736 and te:GetRange()==LOCATION_GRAVE then return true end
+	if mon>1 and code==66200210 and te:GetCode()==EVENT_FLIP then return true end
+	if mon>1 and code==76647978 and te:GetRange()==LOCATION_GRAVE then return true end
+	if mon>1 and (code==83293307 or code==86174055) and te:GetCode()==EVENT_LEAVE_FIELD then return true end
+	if mon>1 and code==92559258 and te:GetCategory()==CATEGORY_SPECIAL_SUMMON then return true end
+	if mon>1 and code==96381979 and te:GetCode()==EVENT_TO_GRAVE then return true end
+	return false
+end
 function aux.PreloadUds()
 	aux.SZoneLimit(0)
 	aux.SZoneLimit(1)
 	aux.MZoneLimit(0)
 	aux.MZoneLimit(1)
-	aux.EffectNegate(0)
-	aux.EffectNegate(1)
+	aux.CannotActivate(0)
+	aux.CannotActivate(1)
 	aux.AdjustRegist()
 	aux.SkipM2()
 	aux.DisableBigDamage()
