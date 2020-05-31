ab4a26cb5793b4e1dc06100d8fd2aee67974c2ba
Joy J <316351048@qq.com>
2020-05-31 11:04:54 +0800

    fix skill 48

--- expansions/script/c100730048.lua
+++ expansions/script/c100730048.lua
@@ -5,7 +5,7 @@ function c100730048.initial_effect(c)
 	aux.RegisterSpeedDuelSkillCardCommon()
 end
 
-function c100730038.filter(c)
+function c100730048.filter(c)
 	return c:IsType(TYPE_TRAP) and c:IsAbleToHand()
 end
 
@@ -13,12 +13,12 @@ function c100730048.con(e,tp)
 	tp=e:GetLabelObject():GetOwner()
 	return aux.SpeedDuelAtMainPhaseCondition(e,tp)
 		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,3,nil,TYPE_TRAP)
-		and Duel.IsExistingMatchingCard(c100730038.filter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c100730048.filter,tp,LOCATION_GRAVE,0,1,nil)
 end
 
 function c100730048.skill(e,tp)
 	tp=e:GetLabelObject():GetOwner()
-	local g1=Duel.GetMatchingGroup(c100730038.filter,tp,LOCATION_GRAVE,0,nil)
+	local g1=Duel.GetMatchingGroup(c100730048.filter,tp,LOCATION_GRAVE,0,nil)
 	if not (g1 and g1:GetCount()>0) then return end
 	local g2=g1:RandomSelect(tp,1)
 	Duel.SendtoHand(g3,nil,REASON_RULE)
