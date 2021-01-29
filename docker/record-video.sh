bb7d11915e3fd7f4396d6d56dd7e1e54b70b4b48
purerosefallen <78877@qq.com>
2021-01-29 14:11:59 +0800

    don't get over 60fps in recording

--- docker/record-video.sh
+++ docker/record-video.sh
@@ -8,7 +8,7 @@ WINDOW_WIDTH=$(grep window_width ./system_user.conf | grep -oP '\d+')
 WINDOW_HEIGHT=$(grep window_height ./system_user.conf | grep -oP '\d+')
 FFMPEG_EXEC=ffmpeg
 rm -f "$VIDEO_NAME"
-./ygopro --auto-record --unlimited-fps --sound-sequence "$SOUND_SEQUENCE_FILE" -r "$REPLAY_NAME" 2>&1 >/dev/null | "$FFMPEG_EXEC" -hwaccel cuda -r 60  -vcodec rawvideo -f rawvideo -pix_fmt rgb24 -s "${WINDOW_WIDTH}x${WINDOW_HEIGHT}" -i - -vcodec h264_nvenc "$TMP_VIDEO_PATH"
+./ygopro --auto-record --sound-sequence "$SOUND_SEQUENCE_FILE" -r "$REPLAY_NAME" 2>&1 >/dev/null | "$FFMPEG_EXEC" -hwaccel cuda -r 60  -vcodec rawvideo -f rawvideo -pix_fmt rgb24 -s "${WINDOW_WIDTH}x${WINDOW_HEIGHT}" -i - -vcodec h264_nvenc "$TMP_VIDEO_PATH"
 FFMPEG_PARAMS=$(python3 ./docker/get-ffmpeg-param.py "$TMP_VIDEO_PATH" "$SOUND_SEQUENCE_FILE" "$VIDEO_NAME")
 echo "Audio FFMpeg params: $FFMPEG_PARAMS"
 bash -c "$FFMPEG_EXEC $FFMPEG_PARAMS"
