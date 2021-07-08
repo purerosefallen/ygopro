d3e10bbbe9c408d84af910bd25d7b153dfc0eccb
purerosefallen <78877@qq.com>
2021-07-08 15:06:03 +0800

    fixmerge

--- gframe/premake4.lua
+++ gframe/premake4.lua
@@ -3,7 +3,6 @@ include "lzma/."
 project "ygopro"
     kind "ConsoleApp"
 
-<<<<<<< HEAD
     local params={
         "DEFAULT_DUEL_RULE",
         "DECKCOUNT_MAIN_MIN",
@@ -16,31 +15,8 @@ project "ygopro"
     for _,param in ipairs(params) do
         local val=os.getenv("YGOPRO_"..param)
         if val and tonumber(val) then defines { param.."="..tonumber(val) } end
-=======
-    files { "**.cpp", "**.cc", "**.c", "**.h" }
-    excludes { "lzma/**", "spmemvfs/**" }
-    includedirs { "../ocgcore" }
-    links { "ocgcore", "clzma", "cspmemvfs", "Irrlicht", "sqlite3", "freetype" }
-    if not LINUX_ALL_STATIC then
-        links { "event" }
-    end
-    if USE_IRRKLANG then
-        defines { "YGOPRO_USE_IRRKLANG" }
-        links { "ikpmp3" }
-        includedirs { "../irrklang/include" }
-        if IRRKLANG_PRO then
-            defines { "IRRKLANG_STATIC" }
-        end
-    end
-    if os.getenv("YGOPRO_COMPAT_MYCARD") then
-        defines { "YGOPRO_COMPAT_MYCARD" }
-    end
-    if os.getenv("YGOPRO_MESSAGE_DEBUG") then
-        defines { "YGOPRO_MESSAGE_DEBUG" }
->>>>>>> master
     end
 
-<<<<<<< HEAD
     files { "gframe.cpp", "config.h",
             "game.cpp", "game.h", "myfilesystem.h",
             "deck_manager.cpp", "deck_manager.h",
@@ -52,12 +28,6 @@ project "ygopro"
     includedirs { "../ocgcore" }
     links { "ocgcore", "clzma", "sqlite3" }
 
-=======
-    configuration "not linux"
-        if LINUX_ALL_STATIC then
-            links { "event" }
-        end
->>>>>>> master
     configuration "windows"
         files "ygopro.rc"
         includedirs { "../event/include", "../sqlite3" }
@@ -72,17 +42,6 @@ project "ygopro"
         if BUILD_SQLITE then
             includedirs { "../sqlite3" }
         end
-<<<<<<< HEAD
-=======
-        if BUILD_FREETYPE then
-            includedirs {"../freetype/include" }
-        end
-    configuration { "not windows", "not macosx" }
-        links "GL"
-    configuration "linux"
-        linkoptions { "-static-libstdc++", "-static-libgcc", "-Wl,-rpath=./lib/" }
-        includedirs { "../irrlicht_linux/include" }
->>>>>>> master
         if BUILD_LUA then
             links { "lua" }
         else
