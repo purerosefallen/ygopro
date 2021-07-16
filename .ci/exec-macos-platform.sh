a0b9c39f11168dfe15da16c4f1be0912c96dddff
purerosefallen <78877@qq.com>
2021-07-16 18:15:15 +0800

    the m1mac

---
+++ .ci/exec-macos-platform.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+set -o errexit
+
+TARGET_YGOPRO_BINARY_PATH=./ygopro-platforms/ygopro-platform-$TARGET_PATFORM
+export YGOPRO_LIBEVENT_STATIC_PATH=$PWD/libevent-stable
+
+git submodule update --init
+
+./premake5 gmake --cc=clang
+cd build
+make config=release -j4
+cd ..
+
+mkdir ygopro-platforms
+mv bin/release/ygopro.app $TARGET_YGOPRO_BINARY_PATH
+
+install_name_tool -change /usr/local/lib/libirrklang.dylib @executable_path/../Frameworks/libirrklang.dylib $TARGET_YGOPRO_BINARY_PATH
+strip $TARGET_YGOPRO_BINARY_PATH
