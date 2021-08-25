7c4e05f6ff22ea8b2c716fe4909a9ed5f563f085
purerosefallen <78877@qq.com>
2021-08-25 16:52:45 +0800

    new ci

--- .ci/pack-windows-7z.sh
+++ .ci/pack-windows-7z.sh
@@ -4,16 +4,15 @@ set -o errexit
 
 ARCHIVE_FILES=(ygopro.exe LICENSE README.md lflist.conf strings.conf system.conf cards.cdb script textures deck single pics replay sound bot.conf Bot.exe WindBot locales fonts)
 
-if [[ "$TARGET_LOCALE" == "zh-CN" && ARCHIVE_SUFFIX != "zst" ]]; then
+if [[ "$TARGET_LOCALE" == "zh-CN" ]]; then
 	ARCHIVE_FILES=("${ARCHIVE_FILES[@]}" update-koishipro)
 fi
 
 # TARGET_LOCALE
-# ARCHIVE_SUFFIX
 TARGET_PLATFORM=win32
 
-apt update && apt -y install tar git zstd
+apt update && apt -y install p7zip-full git
 git submodule update --init
 mkdir dist replay
 
-tar -acf "dist/KoishiPro-$CI_COMMIT_REF_NAME-$TARGET_PLATFORM-$TARGET_LOCALE.tar.$ARCHIVE_SUFFIX" --exclude='.git*' "${ARCHIVE_FILES[@]}"
+7z a -mx9 -xr!.git* dist/KoishiPro-$CI_COMMIT_REF_NAME-win32-$TARGET_LOCALE.7z "${ARCHIVE_FILES[@]}"
