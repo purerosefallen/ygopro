f25417daaf8f59903d0e3c1d9b6e1b9b75f3702f
purerosefallen <78877@qq.com>
2021-09-06 16:18:53 +0800

    always allow custom cards

--- gframe/deck_manager.cpp
+++ gframe/deck_manager.cpp
@@ -73,6 +73,8 @@ const std::unordered_map<int, int>* DeckManager::GetLFListContent(int lfhash) {
 	return nullptr;
 }
 static int checkAvail(int ot, int avail) {
+	if(!!(ot && 0x4))
+		return 0;
 	if((ot & avail) == avail)
 		return 0;
 	if((ot & AVAIL_OCG) && !(avail == AVAIL_OCG))
