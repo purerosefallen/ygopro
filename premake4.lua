02bee0dc8994fab6bb7ea6bb712f851acdb24dff
purerosefallen <78877@qq.com>
2022-01-29 21:46:19 +0800

    test pure x64 build

--- premake4.lua
+++ premake4.lua
@@ -4,6 +4,7 @@ solution "ygo"
     objdir "obj"
 
     configurations { "Release", "Debug" }
+    platforms { "x86_64", "x86" }
     defines { "LUA_COMPAT_5_2" }
     configuration "windows"
         defines { "WIN32", "_WIN32" }
