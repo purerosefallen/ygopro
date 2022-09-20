f9f65073585a1f80dee343cb03be9b3d764bfe3b
purerosefallen <78877@qq.com>
2022-09-20 14:52:32 +0800

    ci for pack

--- .ci/pack-linux.sh
+++ .ci/pack-linux.sh
@@ -2,7 +2,7 @@
 set -x
 set -o errexit
 
-ARCHIVE_FILES=(ygopro LICENSE README.md lib lflist.conf strings.conf system.conf cards.cdb script textures deck single pics replay sound windbot bot bot.conf locales fonts)
+ARCHIVE_FILES=(ygopro LICENSE README.md lib lflist.conf strings.conf system.conf cards.cdb script textures deck single pics replay sound windbot bot bot.conf locales fonts pack)
 
 # TARGET_LOCALE
 # ARCHIVE_SUFFIX
