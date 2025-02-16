75ccd7a41d48e79e418f78b78ede5b72498388de
purerosefallen <78877@qq.com>
2025-02-16 17:51:51 +0800

    use miniaudio

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -1898,6 +1898,7 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event) {
 				return true;
 				break;
 			}
+#ifdef YGOPRO_USE_AUDIO
 			case CHECKBOX_ENABLE_MUSIC: {
 				if(!mainGame->chkEnableMusic->isChecked())
 					soundManager.StopBGM();
@@ -1996,6 +1997,7 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event) {
 				return true;
 				break;
 			}
+#ifdef YGOPRO_USE_AUDIO
 			case SCROLL_VOLUME: {
 				mainGame->gameConf.sound_volume = (double)mainGame->scrSoundVolume->getPos() / 100;
 				mainGame->gameConf.music_volume = (double)mainGame->scrMusicVolume->getPos() / 100;
@@ -2004,6 +2006,7 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event) {
 				return true;
 				break;
 			}
+#endif
 			case SCROLL_TAB_HELPER: {
 				rect<s32> pos = mainGame->tabHelper->getRelativePosition();
 				mainGame->tabHelper->setRelativePosition(recti(0, mainGame->scrTabHelper->getPos() * -1, pos.LowerRightCorner.X, pos.LowerRightCorner.Y));
