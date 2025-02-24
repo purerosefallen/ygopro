0e5806a20e2b00c48cac6f9406c7533f1bdef51b
purerosefallen <78877@qq.com>
2025-02-24 12:00:00 +0800

    add stb_vorbis for ogg support

--- gframe/sound_manager.cpp
+++ gframe/sound_manager.cpp
@@ -40,6 +40,7 @@ void SoundManager::RefershBGMDir(std::wstring path, int scene) {
 			std::wstring filename = path + L"/" + name;
 			BGMList[BGM_ALL].push_back(filename);
 			BGMList[scene].push_back(filename);
+			printf("Found BGM %ls in scene %d\n", filename.c_str(), scene);
 		}
 	});
 }
@@ -223,7 +224,8 @@ void SoundManager::PlayMusic(char* song, bool loop) {
 		BufferIO::DecodeUTF8(song, song_w);
 		ma_sound_init_from_file_w(&engineMusic, song_w, MA_SOUND_FLAG_ASYNC | MA_SOUND_FLAG_STREAM, nullptr, nullptr, &soundBGM);
 #else
-		ma_sound_init_from_file(&engineMusic, song, MA_SOUND_FLAG_ASYNC | MA_SOUND_FLAG_STREAM, nullptr, nullptr, &soundBGM);
+		auto res = ma_sound_init_from_file(&engineMusic, song, MA_SOUND_FLAG_ASYNC | MA_SOUND_FLAG_STREAM, nullptr, nullptr, &soundBGM);
+		printf("res = %d\n", res);
 #endif
 		ma_sound_set_looping(&soundBGM, loop);
 		ma_sound_start(&soundBGM);
@@ -247,6 +249,7 @@ void SoundManager::PlayBGM(int scene) {
 		wchar_t fname[1024];
 		myswprintf(fname, L"./sound/BGM/%ls", name);
 		BufferIO::EncodeUTF8(fname, BGMName);
+		printf("Playing BGM %s in scene %d\n", BGMName, scene);
 		PlayMusic(BGMName, false);
 	}
 #endif
