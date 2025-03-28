2a0c879acfe76d0c1937b705c0f8bae9c1772b60
purerosefallen <78877@qq.com>
2025-03-28 15:51:10 +0800

    patch Makefile on macOS

--- .ci/build-opus.sh
+++ .ci/build-opus.sh
@@ -6,11 +6,23 @@ cd miniaudio
 
 external_built_dir="$PWD/external-built"
 
+is_macos=false
+if [ "$(uname)" = "Darwin" ]; then
+  is_macos=true
+fi
+
+maybe_patch_configure_output() {
+  if $is_macos; then
+    sed -i.bak 's/-force_cpusubtype_ALL//g' Makefile
+  fi
+}
+
 build_single_thing() {
   lib_name="$1"
   cd "external/$lib_name"
   shift
   PKG_CONFIG_PATH="$external_built_dir/lib/pkgconfig" ./configure --prefix="$external_built_dir" --enable-static=yes --enable-shared=no "$@"
+  maybe_patch_configure_output
   make -j$(nproc)
   make install
   cd ../..
