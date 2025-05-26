70108fa06ace3cdadcb95439ff75d06c1776e924
purerosefallen <78877@qq.com>
2025-05-26 15:30:46 +0800

    optimize sqlite3 only non-pro3

--- premake/sqlite3/premake5.lua
+++ premake/sqlite3/premake5.lua
@@ -2,6 +2,7 @@ project "sqlite3"
     kind "StaticLib"
 
     files { "sqlite3.c", "sqlite3.h" }
+if not SERVER_PRO3_SUPPORT then
     defines {
         "SQLITE_DQS=0",
         "SQLITE_DEFAULT_MEMSTATUS=0",
@@ -11,3 +12,4 @@ project "sqlite3"
         "SQLITE_OMIT_PROGRESS_CALLBACK",
         "SQLITE_OMIT_SHARED_CACHE",
     }
+end
