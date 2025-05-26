3bb63facca3ccff3afb7cae76d186f08697056e6
purerosefallen <78877@qq.com>
2025-05-26 11:56:31 +0800

    fix

--- .ci/configure-audio.sh
+++ .ci/configure-audio.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-libs_to_configure=("ogg" "opus")
+libs_to_configure=(
+  "ogg"
+  # "opus"
+)
 
 cd miniaudio/external
 for lib in "${libs_to_configure[@]}"; do
--- premake/miniaudio/premake5.lua
+++ premake/miniaudio/premake5.lua
@@ -117,7 +117,7 @@ project "miniaudio"
                 "HAVE_LRINTF",
                 "OP_HAVE_LRINTF",
             }
-            if not TARGET_MAC_ARM then
+            if not TARGET_MAC_ARM and not IS_ARM then
                 files {
                     "external/opus/celt/x86/pitch_avx.c",
                     "external/opus/celt/x86/pitch_sse.c",
@@ -138,3 +138,4 @@ project "miniaudio"
 
     filter "system:linux"
         links { "dl", "pthread", "m" }
+        buildoptions { "-mavx2", "-mfma" }
